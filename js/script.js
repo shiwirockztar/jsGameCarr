4e2cefc8d74f2358af5196df105d05fb3691d356
shiwirockztar <shiwirockztar@gmail.com>
2022-02-21 10:41:51 -0500

    dinamic page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,50 @@
 // scripts frontend page
-function move() {
-  const element = document.getElementById("myBarB");   
-  let width = 0;
-  const id = setInterval(frame, 10);
-  function frame() {
-    if (width == 100) {
-      clearInterval(id);
-    } else {
-      width++; 
-      element.style.width = width + '%'; 
-    }
+function move0() {
+    const element = document.getElementById("myBar0");   
+    let width = 0;
+    const id = setInterval(frame, 10);
+    function frame() {
+        if (width == 100) {
+          clearInterval(id);
+      } else {
+          width++; 
+          element.style.width = width + '%'; 
+      }
+  }
+}
+
+
+function move1() {
+    const element = document.getElementById("myBar1");   
+    let width = 0;
+    const id = setInterval(frame, 10);
+    function frame() {
+        if (width == 100) {
+          clearInterval(id);
+      } else {
+          width++; 
+          element.style.width = width + '%'; 
+      }
+  }
+}
+function move2() {
+    const element = document.getElementById("myBar2");   
+    let width = 0;
+    const id = setInterval(frame, 10);
+    function frame() {
+        if (width == 100) {
+          clearInterval(id);
+      } else {
+          width++; 
+          element.style.width = width + '%'; 
+      }
   }
 }
 
 let file= `1er lugar para : ${podio[0].name}`
 document.write(file);
+
+
+// let=moven("myBar0");
+// move("myBar1");
+// let tx1="myBar1";
