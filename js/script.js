43fa19bedd944e3b6c81ce31991fd9a951eddf59
shiwirockztar <shiwirockztar@gmail.com>
2022-02-23 22:44:20 -0500

    dinamic page elements

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // scripts frontend page
-function move0() {
-    const element = document.getElementById("myBar0");   
+
+function move() {
+    const element = document.getElementById("myBar");   
     let width = 0;
     const id = setInterval(frame, 10);
     function frame() {
@@ -14,35 +15,43 @@ function move0() {
 }
 
 
-function move1() {
-    const element = document.getElementById("myBar1");   
-    let width = 0;
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (width == 100) {
-          clearInterval(id);
-      } else {
-          width++; 
-          element.style.width = width + '%'; 
-      }
-  }
+
+function dinamic(playerN) {
+    let div =document.createElement("div");
+    div.setAttribute("class","player");
+    let h3=document.createElement("h3");
+    // h3.setAttribute("class","a");
+    h3.innerHTML=playerN;
+    div.appendChild(h3);
+
+
+    let divP =document.createElement("div");
+    divP.setAttribute("class","myProgress");
+    div.appendChild(divP);
+    let divB =document.createElement("div");
+    divB.setAttribute("id","myBar");
+    divP.appendChild(divB);
+    let br =document.createElement("br");
+    div.appendChild(br);
+
+    // document.getElementById("racers").appendChild(div); 
+    let racers=document.querySelector("#racers")     //forma directa de pasar elementos
+    racers.appendChild(div);
 }
-function move2() {
-    const element = document.getElementById("myBar2");   
-    let width = 0;
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (width == 100) {
-          clearInterval(id);
-      } else {
-          width++; 
-          element.style.width = width + '%'; 
-      }
-  }
-}
+
+dinamic(podio[0].name);
+dinamic(podio[1].name);
+dinamic(podio[2].name);
+
+
 
 let file= `1er lugar para : ${podio[0].name}`
 document.write(file);
+
+// document.getElementById("user").innerHTML = podio[0].name;
+// document.getElementById("bot1").innerHTML = podio[1].name;
+// document.getElementById("bot2").innerHTML = podio[2].name;
+
 
 
 // let=moven("myBar0");
