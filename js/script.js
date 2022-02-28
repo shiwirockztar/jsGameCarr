931eda5d951b77f5b9e23e2f2771b16731fe9f6e
shiwirockztar <shiwirockztar@gmail.com>
2022-02-27 22:06:24 -0500

    d a 0.3

--- js/script.js
+++ js/script.js
@@ -16,65 +16,43 @@ function move() {
 
 function progress(argument) {
     const element = document.getElementById("Bar");   
+}
 
+//*************construye la estructura en bloques*************** 
+//**-----------------  de el boton en header ------------------*
+
+let mov=`moveN()`;
+for (chall in challenger) {
+    dinamic(challenger[chall].name,chall);    let text = `, move${chall}()`; mov = mov.concat(text);
 }
 
 
-function move0() {
-    const element = document.getElementById("Bar0");   
-    let width = 0;
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (width == 100) {
-          clearInterval(id);
-      } else {
-          width++; 
-          element.style.width = width + '%'; 
-      }
-  }
-}
+let but=document.createElement("BUTTON");
+but.innerHTML="st4rt";
+but.setAttribute('onclick', "start()"); 
+// let header=document.querySelector("header") 
+let header=document.querySelector("#racers") 
 
+header.appendChild(but);
 
-function move1() {
-    const element = document.getElementById("Bar1");   
-    let width = 0;
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (width == 100) {
-          clearInterval(id);
-      } else {
-          width++; 
-          element.style.width = width + '%'; 
-      }
-  }
-}
-function move2() {
-    const element = document.getElementById("Bar2");   
-    let width = 0;
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (width == 100) {
-          clearInterval(id);
-      } else {
-          width++; 
-          element.style.width = width + '%'; 
-      }
-  }
+// *****************------- dinamic advance--------***************
+function start() {
+    for (i in challenger) { moveN(i);}
 }
 function moveN(N) {
     const element = document.getElementById(`Bar${N}`);   
     let width = 0;
-    const id = setInterval(frame, 10);
+    // console.log(history[N]*100);
+    document.write(history[N]);
+document.write("<br>");
+    const id = setInterval(frame, history[N]*100);
     function frame() {
-        if (width == 100) {
-          clearInterval(id);
-      } else {
-          width++; 
-          element.style.width = width + '%'; 
-      }
-  }
+        if (width == 100) {clearInterval(id);} 
+        else {width++; element.style.width = width + '%'; }
+    }
 }
-
+// --------------------------------------------------------------------
+//**-----------  de la barra de carga para la section----------*
 function dinamic(playerN,N) {
     let div =document.createElement("div");
     div.setAttribute("class","player");
@@ -97,36 +75,29 @@ function dinamic(playerN,N) {
     let racers=document.querySelector("#racers")     //forma directa de pasar elementos
     racers.appendChild(div);
 }
-
+// -*********************************************************************
 // dinamic(podio[0].name,0);
 // dinamic(podio[1].name,1);
 // dinamic(podio[2].name,2);
 
-let mov=`moveN()`;
-for (chall in challenger) {
-    dinamic(challenger[chall].name,chall);
-    // let text = mov;
-    let text = `, move${chall}()`;
-    // let result = mov.concat(text);
-    mov = mov.concat(text);
-
-    // mov = result;
-}
 
 
-let but=document.createElement("BUTTON");
-but.innerHTML="st4rt";
-// but.setAttribute('onclick', mov);
-but.setAttribute('onclick', moveN(0));
-let header=document.querySelector("header") 
-header.appendChild(but);
+
 
 let file= `1er lugar para : ${podio[0].name}`
 document.write(file);
 document.write("<br>");
+let file2= `2do lugar para : ${podio[1].name}`
+document.write(file2);
+document.write("<br>");
+let file3= `3er lugar para : ${podio[2].name}`
+document.write(file3);
+document.write("<br>");
 document.write(history);
 document.write("<br>");
-document.write(mov);
+document.write(history[4]);
+document.write("<br>");
+// document.write(mov);
 
 // document.getElementById("user").innerHTML = podio[0].name;
 // document.getElementById("bot1").innerHTML = podio[1].name;
